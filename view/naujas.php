3be39f272dd9b27d0335093ad8ac8783bbd9e9ff
gobanoff <a.kliosovas@yahoo.com>
2024-02-08 15:31:39 +0200

    sss

--- view/naujas.php
+++ view/naujas.php
@@ -1,11 +1,11 @@
 <?php
 require __DIR__ . '/virsus.php' ?>
-
+ <?php if (isLog()) : ?>
 <form action="<?= URL ?>?route=nauja" method="post">
 
     <button class="a" type="submit">Sukurti nauja uztvanka</button>
 
 </form>
-
+<?php endif ?>
 <?php
 require __DIR__ . '/apacia.php' ?>
